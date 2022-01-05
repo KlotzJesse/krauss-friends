2e36f63c9dfddf2baf0049f8abe53b85166bb88d
Jesse Klotz <jesseklotz2306@gmail.com>
2022-01-05 16:02:08 +0100

    Add NextSeo

--- components/Page.js
+++ components/Page.js
@@ -1,8 +1,10 @@
 import { sbEditable } from "@storyblok/storyblok-editable";
+import { NextSeo } from "next-seo";
 import DynamicComponent from "./DynamicComponent";
 
 const Page = ({ blok }) => (
   <main {...sbEditable(blok)} key={blok._uid}>
+    <NextSeo title={blok.title} description={blok.description} />
     {blok.body
       ? blok.body.map((blok) => (
           <DynamicComponent blok={blok} key={blok._uid} />
