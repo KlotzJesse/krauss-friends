95b8c47d41dcf23d4bd031dac3c5d38df3446067
Jesse Klotz <jesseklotz2306@gmail.com>
2022-01-27 15:59:58 +0100

    Add fathom

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,39 @@
+import * as Fathom from "fathom-client";
 import type { AppProps } from "next/app";
+import { useEffect } from "react";
 import "../styles/globals.css";
 import CookieConsent from "./../components/CookieConsent";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Initialize Fathom when the app loads
+    // Example: yourdomain.com
+    //  - Do not include https://
+    //  - This must be an exact match of your domain.
+    //  - If you're using www. for your domain, make sure you include that here.
+    Fathom.load("YKCURMCH", {
+      includedDomains: [
+        "www.krauss-training.de",
+        "krauss-training.de",
+        "www.krauss-training.com",
+        "krauss-training.com",
+      ],
+    });
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
+    }
+    // Record a pageview when route changes
+    router.events.on("routeChangeComplete", onRouteChangeComplete);
+
+    // Unassign event listener
+    return () => {
+      router.events.off("routeChangeComplete", onRouteChangeComplete);
+    };
+  }, []);
+
   return (
     <div className="">
       <Component {...pageProps} />
