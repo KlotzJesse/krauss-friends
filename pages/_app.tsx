0834030595d5d380a00ae66b39e06573d5f6bb18
Jesse Klotz <jesseklotz2306@gmail.com>
2022-01-27 16:05:23 +0100

    Fix add fathom

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import * as Fathom from "fathom-client";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
 import { useEffect } from "react";
 import "../styles/globals.css";
 import CookieConsent from "./../components/CookieConsent";
