ef13c646ed99141477b87195690123cee20b64fd
Jesse Klotz <jesseklotz2306@gmail.com>
2022-01-28 11:25:24 +0100

    Fix icons

--- components/Targets.jsx
+++ components/Targets.jsx
@@ -22,20 +22,16 @@ const Targets = (props) => {
             </h1>
             <a
               href="https://calendly.com/krauss-friends/kennenlerngespraech"
-              className="flex items-center self-start px-12 py-4 mr-2 font-medium text-white uppercase transition duration-500 ease-in-out transform border-0 rounded-lg shadow-lg primary-cta hover:shadow-inner hover:-translate-y-1 hover:scale-110 focus:outline-none text-md"
+              className="flex items-center self-start px-12 py-4 mr-2 font-medium text-white uppercase transition duration-500 ease-in-out transform bg-orange-400 border-0 rounded-lg shadow-lg hover:shadow-inner hover:-translate-y-1 hover:scale-110 focus:outline-none text-md"
             >
               Erstgespräch reservieren
               <svg
                 className="w-5 h-5 ml-2 fill-current color-white"
-                id="Layer_1"
                 x="0px"
                 y="0px"
                 viewBox="-49 141 512 512"
               >
-                <path
-                  id="XMLID_11_"
-                  d="M-24,422h401.645l-72.822,72.822c-9.763,9.763-9.763,25.592,0,35.355c9.763,9.764,25.593,9.762,35.355,0 l115.5-115.5C460.366,409.989,463,403.63,463,397s-2.634-12.989-7.322-17.678l-115.5-115.5c-9.763-9.762-25.593-9.763-35.355,0 c-9.763,9.763-9.763,25.592,0,35.355l72.822,72.822H-24c-13.808,0-25,11.193-25,25S-37.808,422-24,422z"
-                ></path>
+                <path d="M-24,422h401.645l-72.822,72.822c-9.763,9.763-9.763,25.592,0,35.355c9.763,9.764,25.593,9.762,35.355,0 l115.5-115.5C460.366,409.989,463,403.63,463,397s-2.634-12.989-7.322-17.678l-115.5-115.5c-9.763-9.762-25.593-9.763-35.355,0 c-9.763,9.763-9.763,25.592,0,35.355l72.822,72.822H-24c-13.808,0-25,11.193-25,25S-37.808,422-24,422z"></path>
               </svg>
             </a>
             <p className="self-start pt-3 text-sm text-gray-400 cursor-pointer">
@@ -44,10 +40,7 @@ const Targets = (props) => {
           </div>
           <div className="flex"></div>
           <div className="sticky pt-10 ml-0 md:ml-12 lg:w-1/2 md:pt-0">
-            <div
-              id="ahhh"
-              className="w-full h-full mx-auto space-y-6 sm:container sss"
-            >
+            <div className="w-full h-full mx-auto space-y-6 sm:container sss">
               <div className="relative flex items-center p-4 bg-white rounded-lg shadow-xl">
                 <img
                   alt="team"
@@ -71,7 +64,7 @@ const Targets = (props) => {
                 <img
                   alt="team"
                   className="flex-shrink-0 w-12 h-12 mr-4"
-                  src="https://uxwing.com/wp-content/themes/uxwing/download/42-business-professional-services/interview.svg"
+                  src="https://uxwing.com/wp-content/themes/uxwing/download/24-sport-and-awards/challenge-target.svg"
                 />
                 <div className="flex-grow">
                   <h2 className="font-medium text-gray-900 title-font">
@@ -185,7 +178,7 @@ const Targets = (props) => {
                 <img
                   alt="team"
                   className="flex-shrink-0 w-12 h-12 mr-4"
-                  src="https://uxwing.com/wp-content/themes/uxwing/download/42-business-professional-services/analysis.svg"
+                  src="https://uxwing.com/wp-content/themes/uxwing/download/42-business-professional-services/successful-businessman.svg"
                 />
                 <div className="flex-grow">
                   <h2 className="font-medium text-gray-900 title-font">
