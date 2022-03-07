4c5bfef1af4f36fa73d843b8d0d435c373fe63e6
Jesse Klotz <jesseklotz2306@gmail.com>
2022-03-07 15:52:04 +0100

    Upload

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -7,14 +7,18 @@ const COOKIE_NAME = 'bucket'
 export function middleware(req: NextRequest) {
   // Get the bucket cookie
   const bucket = req.cookies[COOKIE_NAME] || getBucket(HOME_BUCKETS)
-  const url = req.nextUrl.clone()
-  url.searchParams.set("utm_term", "Variante " + bucket)
-  const res = NextResponse.rewrite(url)
-
-  // Add the bucket to cookies if it's not there
-  if (!req.cookies[COOKIE_NAME]) {
-    res.cookie(COOKIE_NAME, bucket)
+  const url = req.nextUrl;
+  if(!url.search.includes("utm_term=Var")) {
+    url.searchParams.set("utm_term", "Var " + bucket);
+    const res = NextResponse.redirect(url);
+  
+    // Add the bucket to cookies if it's not there
+    if (!req.cookies[COOKIE_NAME]) {
+      res.cookie(COOKIE_NAME, bucket)
+    }
+    return res
   }
+  
 
-  return res
+  return NextResponse.next()
 }
