ac3981ceca4b9ad35f0608582b6442fce5df3f28
Jesse Klotz <jesseklotz2306@gmail.com>
2022-03-08 13:54:42 +0100

    Upload

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,15 @@ import "@typeform/embed/build/css/popup.css";
 import * as Fathom from "fathom-client";
 import type { AppProps } from "next/app";
 import dynamic from "next/dynamic";
-import { Router, useRouter } from "next/router";
+import { useRouter } from "next/router";
 import { useEffect } from "react";
 import "../styles/globals.css";
 const CookieConsent = dynamic(() => import("./../components/CookieConsent"));
 
 // Record a pageview when route changes
-Router.events.on("routeChangeComplete", () => {
+/*Router.events.on("routeChangeComplete", () => {
   Fathom.trackPageview();
-});
+});*/
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -19,7 +19,6 @@ function MyApp({ Component, pageProps }: AppProps) {
     // Initialize Fathom when the app loads
     Fathom.load("YKCURMCH", {
       url: "https://cheeky-pleasurable.krauss-training.de/script.js",
-      auto: false,
     });
   }, []);
 
