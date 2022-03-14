8c84ebcb6ac2cca9da2140cd0b9345c943053f1c
Jesse Klotz <jesseklotz2306@gmail.com>
2022-03-14 12:00:16 +0100

    Upload

--- components/ExitIntent.jsx
+++ components/ExitIntent.jsx
@@ -98,7 +98,7 @@ const ExitIntent = ({ page, blok }) => {
               leaveFrom="opacity-100 scale-100"
               leaveTo="opacity-0 scale-95"
             >
-              <div className="inline-block w-full max-w-xl p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
+              <div className="inline-block w-full max-w-2xl p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
                 <div className="absolute top-0 right-0 hidden pt-4 pr-4 sm:block">
                   <button
                     type="button"
