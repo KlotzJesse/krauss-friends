4802efe0b4e202504770d70f02fc10e37fabf86b
Jesse Klotz <jesseklotz2306@gmail.com>
2022-03-15 11:37:20 +0100

    Upload

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,7 @@ class MyDocument extends Document {
       <Html lang="de">
         <Head>
           <link
-            href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
+            href="https://fonts.googleapis.com/css2?family=Inter"
             rel="stylesheet"
           />
           <script
