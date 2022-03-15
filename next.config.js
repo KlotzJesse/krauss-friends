3e138b510b1ef813ee1e8440d05d76e3ca24ef22
Jesse Klotz <jesseklotz2306@gmail.com>
2022-03-15 10:59:40 +0100

    Upload

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   swcMinify: true,
-  reactStrictMode: true,
+  reactStrictMode: false,
   images: {
     formats: ["image/avif", "image/webp"],
     domains: ["www.krauss-friends.com", "i.ibb.co", "image.jimcdn.com"],
