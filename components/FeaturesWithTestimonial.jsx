f288cdfbb98ef0074c5cd11f99f4400619a4c4d3
Jesse Klotz <jesseklotz2306@gmail.com>
2022-03-16 12:20:43 +0100

    Upload

--- components/FeaturesWithTestimonial.jsx
+++ components/FeaturesWithTestimonial.jsx
@@ -81,12 +81,11 @@ const FeaturesWithTestimonial = ({ page }) => {
               </blockquote>
             </div>
           </div>
-          <div className="mt-12 sm:mt-16 lg:mt-0 hidden md:block">
+          <div className="hidden mt-12 sm:mt-16 lg:mt-0 md:block">
             <div className="pl-4 -mr-48 sm:pl-6 md:-mr-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
               <img
                 className="w-full shadow-xl rounded-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
-                src="https://i.ibb.co/sPYMMpT/inbox-app-screenshot-2-min.png"
-                alt="Inbox user interface"
+                src="/Funnel.png"
               />
             </div>
           </div>
@@ -126,7 +125,7 @@ const FeaturesWithTestimonial = ({ page }) => {
               </div>
             </div>
           </div>
-          <div className="mt-12 sm:mt-16 lg:mt-0 lg:col-start-1 hidden md:block">
+          <div className="hidden mt-12 sm:mt-16 lg:mt-0 lg:col-start-1 md:block">
             <div className="pr-4 -ml-48 sm:pr-6 md:-ml-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
               <img
                 className="w-full shadow-xl rounded-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:right-0 lg:h-full lg:w-auto lg:max-w-none"
