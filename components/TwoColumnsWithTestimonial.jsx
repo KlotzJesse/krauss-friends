06914907361b0612b435508e78856f9cbe98f63f
Jesse Klotz <jesseklotz2306@gmail.com>
2022-03-16 12:40:08 +0100

    Upload

--- components/TwoColumnsWithTestimonial.jsx
+++ components/TwoColumnsWithTestimonial.jsx
@@ -2,75 +2,68 @@ import React from "react";
 
 const TwoColumnsWithTestimonial = (props) => {
   return (
-    <div className="py-16  overflow-hidden">
-      <div className="max-w-7xl mx-auto px-4 space-y-8 sm:px-6 lg:px-8">
-        <div className="text-base max-w-prose mx-auto lg:max-w-none">
-          <h2 className="text-base text-primary-600 font-semibold tracking-wide uppercase">
-            Transactions
+    <div className="py-16 overflow-hidden">
+      <div className="px-4 mx-auto space-y-8 max-w-7xl sm:px-6 lg:px-8">
+        <div className="mx-auto text-base max-w-prose lg:max-w-none">
+          <h2 className="text-base font-semibold tracking-wide uppercase text-primary-600">
+            Mehr Termine, mehr Umsatz
           </h2>
-          <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-            What makes us different
+          <p className="mt-2 text-3xl font-extrabold leading-8 tracking-tight text-gray-900 sm:text-4xl">
+            Wie Sie mehr Termine erhalten
           </p>
         </div>
-        <div className="relative z-10 text-base max-w-prose mx-auto lg:max-w-5xl lg:mx-0 lg:pr-72">
+        <div className="relative z-10 mx-auto text-base max-w-prose lg:max-w-5xl lg:mx-0 lg:pr-72">
           <p className="text-lg text-gray-500">
-            Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum
-            urna sed consectetur neque tristique pellentesque. Blandit amet, sed
-            aenean erat arcu morbi. Cursus faucibus nunc nisl netus morbi vel
-            porttitor vitae ut. Amet vitae fames senectus vitae.
+            Es ist schwierig, mit dem richtigen Entscheidungsträger in einem
+            Unternehmen in Kontakt zu kommen. Sie verbringen Stunden mit
+            Recherchen und Kaltakquise, nur um ein &quot;Nein&quot; zu erhalten
+            oder an jemanden weitergeleitet zu werden, der Ihnen nicht
+            weiterhelfen kann.
           </p>
         </div>
         <div className="lg:grid lg:grid-cols-2 lg:gap-8 lg:items-start">
           <div className="relative z-10">
-            <div className="prose prose-primary text-gray-500 mx-auto lg:max-w-none">
+            <div className="mx-auto prose text-gray-500 prose-primary lg:max-w-none">
               <p>
-                Sollicitudin tristique eros erat odio sed vitae, consequat
-                turpis elementum. Lorem nibh vel, eget pretium arcu vitae. Eros
-                eu viverra donec ut volutpat donec laoreet quam urna.
+                Zeit ist Geld, und wenn Sie Ihre Zeit nicht sinnvoll nutzen,
+                entgehen Ihnen potenzielle Aufträge. Sie müssen in der Lage
+                sein, schnell und einfach mit der richtigen Person in Kontakt zu
+                treten, um einen Verkauf abzuschließen.
               </p>
               <ul role="list">
-                <li>Quis elit egestas venenatis mattis dignissim.</li>
                 <li>
-                  Cras cras lobortis vitae vivamus ultricies facilisis tempus.
+                  Erhöhen Sie Ihre Erfolgschancen mit qualifizierten Terminen
                 </li>
-                <li>Orci in sit morbi dignissim metus diam arcu pretium.</li>
+                <li>
+                  Zugang zu einer Datenbank mit über 300.000
+                  Entscheidungsträgern
+                </li>
+                <li>Wir kennen Ihre Kunden und haben alle Informationen</li>
               </ul>
+
+              <h3>Erhalten Sie jetzt zielsichere Termine</h3>
               <p>
-                Rhoncus nisl, libero egestas diam fermentum dui. At quis
-                tincidunt vel ultricies. Vulputate aliquet velit faucibus
-                semper. Pellentesque in venenatis vestibulum consectetur nibh
-                id. In id ut tempus egestas. Enim sit aliquam nec, a. Morbi enim
-                fermentum lacus in. Viverra.
-              </p>
-              <h3>We’re here to help</h3>
-              <p>
-                Tincidunt integer commodo, cursus etiam aliquam neque, et.
-                Consectetur pretium in volutpat, diam. Montes, magna cursus
-                nulla feugiat dignissim id lobortis amet. Laoreet sem est
-                phasellus eu proin massa, lectus. Diam rutrum posuere donec
-                ultricies non morbi. Mi a platea auctor mi.
+                Der Terminservice ist die Lösung für Sie. Wir verfügen über eine
+                Datenbank mit über 300.000 Entscheidungsträgern, mit denen wir
+                Sie schnell und einfach verbinden können. Wir wissen alles über
+                unsere Kunden, so dass Sie keine Zeit mit der Suche nach ihnen
+                verschwenden müssen, und wir kontaktieren interessierte Kunden
+                in Ihrem Namen, so dass Sie sich auf den Verkauf konzentrieren
+                können.
               </p>
             </div>
-            <div className="mt-10 flex text-base max-w-prose mx-auto lg:max-w-none">
+            <div className="flex mx-auto mt-10 text-base max-w-prose lg:max-w-none">
               <div className="rounded-md shadow">
                 <a
                   href="#"
-                  className="w-full flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-primary-600 hover:bg-primary-700"
+                  className="flex items-center justify-center w-full px-5 py-3 text-base font-medium text-white border border-transparent rounded-md bg-primary-600 hover:bg-primary-700"
                 >
-                  Contact sales
-                </a>
-              </div>
-              <div className="rounded-md shadow ml-4">
-                <a
-                  href="#"
-                  className="w-full flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-primary-600 bg-white hover:bg-gray-50"
-                >
-                  Learn more
+                  Kennenlerngespräch vereinbaren
                 </a>
               </div>
             </div>
           </div>
-          <div className="mt-12 relative text-base max-w-prose mx-auto lg:mt-0 lg:max-w-none">
+          <div className="relative mx-auto mt-12 text-base max-w-prose lg:mt-0 lg:max-w-none">
             <svg
               className="absolute top-0 right-0 -mt-20 -mr-20 lg:top-auto lg:right-auto lg:bottom-1/2 lg:left-1/2 lg:mt-0 lg:mr-0 xl:top-0 xl:right-0 xl:-mt-20 xl:-mr-20"
               width={404}
@@ -105,15 +98,15 @@ const TwoColumnsWithTestimonial = (props) => {
               />
             </svg>
             <blockquote className="relative bg-white rounded-lg shadow-lg">
-              <div className="rounded-t-lg px-6 py-8 sm:px-10 sm:pt-10 sm:pb-8">
-                <img
+              <div className="px-6 py-8 rounded-t-lg sm:px-10 sm:pt-10 sm:pb-8">
+                {/*<img
                   src="https://tailwindui.com/img/logos/workcation-logo-indigo-600-mark-gray-800-and-indigo-600-text.svg"
                   alt="Workcation"
                   className="h-8"
-                />
-                <div className="relative text-lg text-gray-700 font-medium mt-8">
+  />*/}
+                <div className="relative mt-8 text-lg font-medium text-gray-700">
                   <svg
-                    className="absolute top-0 left-0 transform -translate-x-3 -translate-y-2 h-8 w-8 text-gray-200"
+                    className="absolute top-0 left-0 w-8 h-8 text-gray-200 transform -translate-x-3 -translate-y-2"
                     fill="currentColor"
                     viewBox="0 0 32 32"
                     aria-hidden="true"
@@ -121,26 +114,24 @@ const TwoColumnsWithTestimonial = (props) => {
                     <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
                   </svg>
                   <p className="relative">
-                    Tincidunt integer commodo, cursus etiam aliquam neque, et.
-                    Consectetur pretium in volutpat, diam. Montes, magna cursus
-                    nulla feugiat dignissim id lobortis amet. Laoreet sem est
-                    phasellus eu proin massa, lectus.
+                    Warum haben wir eine bessere Abschlussquote bei Ihren
+                    Terminen als bei unseren eigenen?
                   </p>
                 </div>
               </div>
-              <cite className="relative flex items-center sm:items-start bg-primary-600 rounded-b-lg not-italic py-5 px-6 sm:py-5 sm:pl-12 sm:pr-10 sm:mt-10">
-                <div className="relative rounded-full border-2 border-white sm:absolute sm:top-0 sm:transform sm:-translate-y-1/2">
+              <cite className="relative flex items-center px-6 py-5 not-italic rounded-b-lg sm:items-start bg-primary-600 sm:py-5 sm:pl-12 sm:pr-10 sm:mt-10">
+                <div className="relative border-2 border-white rounded-full sm:absolute sm:top-0 sm:transform sm:-translate-y-1/2">
                   <img
-                    className="w-12 h-12 sm:w-20 sm:h-20 rounded-full bg-primary-300"
-                    src="https://images.unsplash.com/photo-1500917293891-ef795e70e1f6?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2.5&w=160&h=160&q=80"
+                    className="w-12 h-12 rounded-full sm:w-20 sm:h-20 bg-primary-300"
+                    src="https://renderapi.s3.amazonaws.com/eJQVu3p1G.png"
                     alt=""
                   />
                 </div>
-                <span className="relative ml-4 text-primary-300 font-semibold leading-6 sm:ml-24 sm:pl-1">
-                  <p className="text-white font-semibold sm:inline">
-                    Judith Black
+                <span className="relative ml-4 font-semibold leading-6 text-primary-300 sm:ml-24 sm:pl-1">
+                  <p className="font-semibold text-white sm:inline">
+                    Geschäftsführer
                   </p>{" "}
-                  <p className="sm:inline">CEO at Workcation</p>
+                  <p className="sm:inline">im Gastgewerbe</p>
                 </span>
               </cite>
             </blockquote>
