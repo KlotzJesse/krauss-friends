7fdc1eec5d0d39e4721b7ccc14574b8fe7468ae3
Jesse Klotz <jesseklotz2306@gmail.com>
2022-03-30 16:00:22 +0200

    Upload

--- components/CloudProjektLP.jsx
+++ components/CloudProjektLP.jsx
@@ -14,10 +14,18 @@ import {
   ViewGridIcon,
   XIcon,
 } from "@heroicons/react/outline";
+import { ChevronRightIcon } from "@heroicons/react/solid";
 import { createPopup } from "@typeform/embed";
 import { trackGoal } from "fathom-client";
 import { Fragment, useRef } from "react";
 import { openPopupWidget } from "react-calendly";
+
+const navigation = [
+  { name: "Product", href: "#" },
+  { name: "Features", href: "#" },
+  { name: "Marketplace", href: "#" },
+  { name: "Company", href: "#" },
+];
 
 const features = [
   {
@@ -102,7 +110,7 @@ export default function CloudProjektLP({ blok, page }) {
                   <span className="sr-only">KRAUSS Training</span>
                   <img
                     className="w-auto h-8 sm:h-10"
-                    src="/KRAUSS Logo Blau.svg"
+                    src="/gastronomyworld.svg"
                     alt=""
                   />
                 </a>
@@ -165,11 +173,7 @@ export default function CloudProjektLP({ blok, page }) {
                 <div className="px-5 pt-5 pb-6">
                   <div className="flex items-center justify-between">
                     <div>
-                      <img
-                        className="w-auto h-8"
-                        src="/KRAUSS Blau Logo.svg"
-                        alt="KRAUSS Training"
-                      />
+                      <img className="w-auto h-8" src="/gastronomyworld.svg" />
                     </div>
                     <div className="-mr-2">
                       <Popover.Button className="inline-flex items-center justify-center p-2 text-gray-400 bg-white rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
@@ -222,74 +226,201 @@ export default function CloudProjektLP({ blok, page }) {
           </Transition>
         </Popover>
 
-        <main className="lg:relative h-full">
-          <img
-            className="absolute top-0 left-0 object-cover w-full h-full"
-            src="https://i.postimg.cc/PxRFMZgn/Design-ohne-Titel-13.png"
-            alt=""
-          />
-          <div className="absolute top-0 left-0 w-full h-full bg-white opacity-90"></div>
-          <div className="relative z-10 w-full pt-16 pb-20 mx-auto text-center max-w-5xl lg:pt-36 lg:text-left sm:px-6 px-4">
-            <h1 className="text-2xl font-medium tracking-tight  text-secondary-500">
-              Für Vertriebs- und Verkaufsleiter:
-            </h1>
-            <p className="text-[2.6em] font-bold tracking-tight pt-5">
-              Mehr Abschlüsse und Neukunden durch ein hochmotiviertes
-              Vertriebsteam.
-            </p>
-            <div className="flex space-x-10 pt-5 w-full">
-              <div className="w-[60%]">
-                <img
-                  src="https://a.storyblok.com/f/141545/1920x1080/eb6dacdfff/krauss-interview.jpg"
-                  className="shadow-xl"
-                />
-              </div>
-              <div className="w-[40%]">
-                <ul className="list-reset text-black mb-8 text-grey-darker rounded">
-                  <li className="flex items-center mb-3">
-                    <CheckIcon className="flex-none w-7 mr-2 text-secondary-500 " />
-                    <p>
-                      <b className="underline">Höhere Abschlussquoten</b>, weil
-                      wir Ihre Mitarbeiter zu professionellen
-                      &quot;Verkaufstalenten&quot; machen
-                    </p>
-                  </li>
-                  <li className="flex items-center mb-3">
-                    <CheckIcon className="flex-none w-7 mr-2 text-secondary-500 " />
-
-                    <p>
-                      <b className="underline">
-                        Steigerung der Gesamtperformance
-                      </b>
-                      , da Sie nicht mehr abhängig von einzelnen Superstars im
-                      Team sind.
-                    </p>
-                  </li>
-                  <li className="flex items-center mb-3">
-                    <CheckIcon className="flex-none w-7 mr-2 text-secondary-500 " />
-                    <p>
-                      <b className="underline">
-                        Preise erfolgreich durchsetzen
-                      </b>
-                      , da nirgends soviel Ertrag gewonnen oder verloren wird
-                    </p>
-                  </li>
-                </ul>
-                <button
-                  onClick={onClick}
-                  className="bg-secondary-500 px-10 py-3 text-white transform hover:scale-110 transition-all ease-in-out duration-300 w-full"
+        <div className="relative bg-gray-800 overflow-hidden">
+          <div
+            className="hidden sm:block sm:absolute sm:inset-0"
+            aria-hidden="true"
+          >
+            <svg
+              className="absolute bottom-0 right-0 transform translate-x-1/2 mb-48 text-gray-700 lg:top-0 lg:mt-28 lg:mb-0 xl:transform-none xl:translate-x-0"
+              width={364}
+              height={384}
+              viewBox="0 0 364 384"
+              fill="none"
+            >
+              <defs>
+                <pattern
+                  id="eab71dd9-9d7a-47bd-8044-256344ee00d0"
+                  x={0}
+                  y={0}
+                  width={20}
+                  height={20}
+                  patternUnits="userSpaceOnUse"
                 >
-                  <span className="text-[1.375em] font-bold">
-                    Jetzt Erstgespräch sichern!
-                  </span>
-                  <span className="block text-gray-200 font-medium">
-                    kostenfrei & unverbindlich
-                  </span>
-                </button>
-              </div>
-            </div>
+                  <rect x={0} y={0} width={4} height={4} fill="currentColor" />
+                </pattern>
+              </defs>
+              <rect
+                width={364}
+                height={384}
+                fill="url(#eab71dd9-9d7a-47bd-8044-256344ee00d0)"
+              />
+            </svg>
           </div>
-        </main>
+          <div className="relative pt-6 pb-16 sm:pb-24">
+            <main className="mt-16 sm:mt-24">
+              <div className="mx-auto max-w-7xl">
+                <div className="lg:grid lg:grid-cols-12 lg:gap-8">
+                  <div className="px-4 sm:px-6 sm:text-center md:max-w-2xl md:mx-auto lg:col-span-6 lg:text-left lg:flex lg:items-center">
+                    <div>
+                      <a
+                        href="#"
+                        className="inline-flex items-center text-white bg-gray-900 rounded-full p-1 pr-2 sm:text-base lg:text-sm xl:text-base hover:text-gray-200"
+                      >
+                        <span className="px-3 py-0.5 text-white text-xs font-semibold leading-5 uppercase tracking-wide bg-indigo-500 rounded-full">
+                          We're hiring
+                        </span>
+                        <span className="ml-4 text-sm">
+                          Visit our careers page
+                        </span>
+                        <ChevronRightIcon
+                          className="ml-2 w-5 h-5 text-gray-500"
+                          aria-hidden="true"
+                        />
+                      </a>
+                      <h1 className="mt-4 text-4xl tracking-tight font-extrabold text-white sm:mt-5 sm:leading-none lg:mt-6 lg:text-5xl xl:text-6xl">
+                        <span className="md:block">Data to enrich your</span>{" "}
+                        <span className="text-indigo-400 md:block">
+                          online business
+                        </span>
+                      </h1>
+                      <p className="mt-3 text-base text-gray-300 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
+                        Anim aute id magna aliqua ad ad non deserunt sunt. Qui
+                        irure qui lorem cupidatat commodo. Elit sunt amet fugiat
+                        veniam occaecat fugiat aliqua ad ad non deserunt sunt.
+                      </p>
+                      <p className="mt-8 text-sm text-white uppercase tracking-wide font-semibold sm:mt-10">
+                        Used by
+                      </p>
+                      <div className="mt-5 w-full sm:mx-auto sm:max-w-lg lg:ml-0">
+                        <div className="flex flex-wrap items-start justify-between">
+                          <div className="flex justify-center px-1">
+                            <img
+                              className="h-9 sm:h-10"
+                              src="https://tailwindui.com/img/logos/tuple-logo-gray-400.svg"
+                              alt="Tuple"
+                            />
+                          </div>
+                          <div className="flex justify-center px-1">
+                            <img
+                              className="h-9 sm:h-10"
+                              src="https://tailwindui.com/img/logos/workcation-logo-gray-400.svg"
+                              alt="Workcation"
+                            />
+                          </div>
+                          <div className="flex justify-center px-1">
+                            <img
+                              className="h-9 sm:h-10"
+                              src="https://tailwindui.com/img/logos/statickit-logo-gray-400.svg"
+                              alt="StaticKit"
+                            />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="mt-16 sm:mt-24 lg:mt-0 lg:col-span-6">
+                    <div className="bg-white sm:max-w-md sm:w-full sm:mx-auto sm:rounded-lg sm:overflow-hidden">
+                      <div className="px-4 py-8 sm:px-10">
+                        <div className="mt-6">
+                          <form action="#" method="POST" className="space-y-6">
+                            <div>
+                              <label htmlFor="name" className="sr-only">
+                                Full name
+                              </label>
+                              <input
+                                type="text"
+                                name="name"
+                                id="name"
+                                autoComplete="name"
+                                placeholder="Full name"
+                                required
+                                className="block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-gray-300 rounded-md"
+                              />
+                            </div>
+
+                            <div>
+                              <label
+                                htmlFor="mobile-or-email"
+                                className="sr-only"
+                              >
+                                Mobile number or email
+                              </label>
+                              <input
+                                type="text"
+                                name="mobile-or-email"
+                                id="mobile-or-email"
+                                autoComplete="email"
+                                placeholder="Mobile number or email"
+                                required
+                                className="block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-gray-300 rounded-md"
+                              />
+                            </div>
+
+                            <div>
+                              <label htmlFor="password" className="sr-only">
+                                Password
+                              </label>
+                              <input
+                                id="password"
+                                name="password"
+                                type="password"
+                                placeholder="Password"
+                                autoComplete="current-password"
+                                required
+                                className="block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-gray-300 rounded-md"
+                              />
+                            </div>
+
+                            <div className="max-w-sm mx-auto">
+                              <button
+                                onClick={onClick}
+                                className="bg-secondary-500 px-10 py-3 text-white transform hover:scale-110 transition-all ease-in-out duration-300 w-full"
+                              >
+                                <span className="text-[1.375em] font-bold">
+                                  Jetzt Erstgespräch sichern!
+                                </span>
+                                <span className="block text-gray-200 font-medium">
+                                  kostenfrei & unverbindlich
+                                </span>
+                              </button>
+                            </div>
+                          </form>
+                        </div>
+                      </div>
+                      <div className="px-4 py-6 bg-gray-50 border-t-2 border-gray-200 sm:px-10">
+                        <p className="text-xs leading-5 text-gray-500">
+                          By signing up, you agree to our{" "}
+                          <a
+                            href="#"
+                            className="font-medium text-gray-900 hover:underline"
+                          >
+                            Terms
+                          </a>
+                          ,{" "}
+                          <a
+                            href="#"
+                            className="font-medium text-gray-900 hover:underline"
+                          >
+                            Data Policy
+                          </a>{" "}
+                          and{" "}
+                          <a
+                            href="#"
+                            className="font-medium text-gray-900 hover:underline"
+                          >
+                            Cookies Policy
+                          </a>
+                          .
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </main>
+          </div>
+        </div>
       </div>
       <div className=" bg-slate-900 text-white">
         <div className="max-w-5xl mx-auto text-center pb-20 pt-20">
