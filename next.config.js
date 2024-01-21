28362bbd18f6f5dd9f2318dc0c9c04030981d1e4
KlotzJesse <jesseklotz2306@gmail.com>
2024-01-21 16:52:22 +0100

    fix: fix it finally

--- next.config.js
+++ next.config.js
@@ -32,12 +32,12 @@ module.exports = {
           destination: "/neukundengewinnung",
         },
         {
-          source: "/",
+          source: "/*",
           has: [{ type: "host", value: "krauss-friends.com" }],
           destination: "/vertrieb-trainieren",
         },
         {
-          source: "/",
+          source: "/*",
           has: [{ type: "host", value: "www.krauss-friends.com" }],
           destination: "/vertrieb-trainieren",
         },
